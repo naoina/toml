71da4b152ef0b2058b6d82ab3cad47da9260ce8b
Tim Raymond <tim@timraymond.com>
2016-01-06 12:04:51 -0500

    Actually fix map iteration order problem
    I was sorting the collection, but I wasn't actually using it!

--- encode.go
+++ encode.go
@@ -176,12 +176,19 @@ func encodeValue(buf []byte, prefix, name string, fv reflect.Value, inArray, arr
 		keys := fv.MapKeys()
 		sortedKeys := make([]string, 0, len(keys))
 		for _, key := range keys {
-			sortedKeys = append(sortedKeys, key.String())
+			var keyStr string
+			switch key.Interface().(type) {
+			case fmt.Stringer:
+				keyStr = key.String()
+			case string:
+				keyStr = key.Interface().(string)
+			}
+			sortedKeys = append(sortedKeys, keyStr)
 		}
 		sort.Strings(sortedKeys)
 
 		var err error
-		for _, key := range keys {
+		for _, key := range sortedKeys {
 			buf, err = encodeValue(buf, name, key.String(), fv.MapIndex(key), inArray, arrayTable)
 			if err != nil {
 				return nil, err
