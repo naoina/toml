naoina/toml

commit f948cc73fe60db828f98ff6240af371db34e8a79

Prevent decoding of struct fields with toml:"-" tag
Also restore `nice' quotes.

util.go
@@ -1,6 +1,7 @@
 package toml
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 	"unicode"
@@ -34,15 +35,19 @@ const (
 	fieldTagName = "toml"
 )
 
-func findField(rv reflect.Value, name string) (field reflect.Value, fieldName string, found bool) {
+func findField(rv reflect.Value, name string) (field reflect.Value, fieldName string, err error) {
 	rt := rv.Type()
 	for i := 0; i < rt.NumField(); i++ {
 		ft := rt.Field(i)
 		if ft.PkgPath != "" && !ft.Anonymous {
 			continue
 		}
-		if col, _ := extractTag(ft.Tag.Get(fieldTagName)); col == name {
-			return rv.Field(i), ft.Name, true
+		col, _ := extractTag(ft.Tag.Get(fieldTagName))
+		if col == "-" {
+			return field, "", fmt.Errorf("field corresponding to `%s' in %v cannot be set through TOML", name, rv.Type())
+		}
+		if col == name {
+			return rv.Field(i), ft.Name, nil
 		}
 	}
 	for _, name := range []string{
@@ -51,10 +56,10 @@ func findField(rv reflect.Value, name string) (field reflect.Value, fieldName st
 		strings.ToUpper(name),
 	} {
 		if field := rv.FieldByName(name); field.IsValid() {
-			return field, name, true
+			return field, name, nil
 		}
 	}
-	return field, "", false
+	return field, "", fmt.Errorf("field corresponding to `%s' is not defined in %v", name, rv.Type())
 }
 
 func extractTag(tag string) (col, rest string) {

decode_test.go
@@ -607,6 +607,9 @@ func TestUnmarshal_WithTable(t *testing.T) {
 		X X
 		A A
 	}
+	type testIgnoredFieldStruct struct {
+		Ignored string `toml:"-"`
+	}
 	type testQuotedKeyStruct struct {
 		Dog struct {
 			TaterMan struct {
@@ -764,12 +767,17 @@ d = 2`, nil,
 		{`[.]`, lineError(1, errParse), &testStruct{}},
 		{` = "no key name" # not allowed`, lineError(1, errParse), &testStruct{}},
 		{
+			data:   `ignored = "value"`,
+			err:    lineError(1, fmt.Errorf("field corresponding to `ignored' in toml.testIgnoredFieldStruct cannot be set through TOML")),
+			expect: &testIgnoredFieldStruct{},
+		},
+		{
 			data: `
 [a]
 d = 2
 y = 3
 `,
-			err:    lineErrorField(4, "toml.testStruct.A", fmt.Errorf("field corresponding to 'y' is not defined in toml.A")),
+			err:    lineErrorField(4, "toml.testStruct.A", fmt.Errorf("field corresponding to `y' is not defined in toml.A")),
 			expect: &testStruct{},
 		},
 	})

decode.go
@@ -154,9 +154,9 @@ func unmarshalTable(rv reflect.Value, t *ast.Table, toplevelMap bool) error {
 	switch {
 	case rv.Kind() == reflect.Struct:
 		for key, fieldAst := range t.Fields {
-			fv, fieldName, found := findField(rv, key)
-			if !found {
-				return lineError(fieldLineNumber(fieldAst), fmt.Errorf("field corresponding to '%s' is not defined in %v", key, rv.Type()))
+			fv, fieldName, err := findField(rv, key)
+			if err != nil {
+				return lineError(fieldLineNumber(fieldAst), err)
 			}
 			if err := unmarshalField(fv, fieldAst); err != nil {
 				return lineErrorField(fieldLineNumber(fieldAst), rv.Type().String()+"."+fieldName, err)

commit fb2dba6d4f13c186a0761b2ffd9d82f4aba4005a

Fix ignored field handling
The change in f948cc7 broke decoding of structs where any field had
the toml:"-" tag.

util.go
@@ -35,28 +35,30 @@ const (
 	fieldTagName = "toml"
 )
 
+func matchField(fieldName, name string) bool {
+	return fieldName == strings.Title(name) ||
+		fieldName == toCamelCase(name) ||
+		fieldName == strings.ToUpper(name)
+}
+
 func findField(rv reflect.Value, name string) (field reflect.Value, fieldName string, err error) {
 	rt := rv.Type()
 	for i := 0; i < rt.NumField(); i++ {
 		ft := rt.Field(i)
+		// skip unexported fields
 		if ft.PkgPath != "" && !ft.Anonymous {
 			continue
 		}
+
 		col, _ := extractTag(ft.Tag.Get(fieldTagName))
-		if col == "-" {
-			return field, "", fmt.Errorf("field corresponding to `%s' in %v cannot be set through TOML", name, rv.Type())
-		}
-		if col == name {
+		if col != "-" && name == col {
 			return rv.Field(i), ft.Name, nil
 		}
-	}
-	for _, name := range []string{
-		strings.Title(name),
-		toCamelCase(name),
-		strings.ToUpper(name),
-	} {
-		if field := rv.FieldByName(name); field.IsValid() {
-			return field, name, nil
+		if matchField(ft.Name, name) {
+			if col == "-" {
+				return field, "", fmt.Errorf("field corresponding to `%s' in %v cannot be set through TOML", name, rv.Type())
+			}
+			return rv.Field(i), ft.Name, nil
 		}
 	}
 	return field, "", fmt.Errorf("field corresponding to `%s' is not defined in %v", name, rv.Type())

decode_test.go
@@ -772,6 +772,12 @@ d = 2`, nil,
 			expect: &testIgnoredFieldStruct{},
 		},
 		{
+			data:   `"-" = "value"`,
+			err:    lineError(1, fmt.Errorf("field corresponding to `\"-\"' is not defined in toml.testIgnoredFieldStruct")),
+			expect: &testIgnoredFieldStruct{},
+		},
+
+		{
 			data: `
 [a]
 d = 2