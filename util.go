naoina/toml

commit d95c95d4f973c5112aa5c12d5ce7b4091b1e35b8

Simplify field matching logic
findField, which runs for each key in the input, looked for a matching
named defined through the struct tag and then tried to come up with a
struct field name by transforming the key name in various ways.

For the example in #28, the key is 'api_key'. and findField tried
'Api_key', 'ApiKey', and 'API_KEY'. None of these match the field
'APIKey', which is created by the encoder using ToSnakeCase and its
built-in abbreviation table.

The new code has linear complexity and ensures that key names defined
through a struct tag are enforced. It was previously possible to set
such fields through their auto-mapped name. This is also prep-work for
user-defined field mapping functions.

For #28, the code now creates a cache containing 'apikey' as the mapped
name for the 'APIKey' field. The input key 'api_key' is also mapped to
'apikey' by the same function.

Fixes #28

util.go
@@ -4,64 +4,49 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
-	"unicode"
 )
 
-// toCamelCase returns a copy of the string s with all Unicode letters mapped to their camel case.
-// It will convert to upper case previous letter of '_' and first letter, and remove letter of '_'.
-func toCamelCase(s string) string {
-	if s == "" {
-		return ""
-	}
-	result := make([]rune, 0, len(s))
-	upper := false
-	for _, r := range s {
-		if r == '_' {
-			upper = true
-			continue
-		}
-		if upper {
-			result = append(result, unicode.ToUpper(r))
-			upper = false
-			continue
-		}
-		result = append(result, r)
-	}
-	result[0] = unicode.ToUpper(result[0])
-	return string(result)
+const fieldTagName = "toml"
+
+// fieldCache maps normalized field names to their position in a struct.
+type fieldCache map[string]fieldInfo
+
+type fieldInfo struct {
+	index   []int
+	name    string
+	ignored bool
 }
 
-const (
-	fieldTagName = "toml"
-)
-
-func matchField(fieldName, name string) bool {
-	return fieldName == strings.Title(name) ||
-		fieldName == toCamelCase(name) ||
-		fieldName == strings.ToUpper(name)
-}
-
-func findField(rv reflect.Value, name string) (field reflect.Value, fieldName string, err error) {
-	rt := rv.Type()
+func makeFieldCache(rt reflect.Type) fieldCache {
+	fc := make(fieldCache)
 	for i := 0; i < rt.NumField(); i++ {
 		ft := rt.Field(i)
 		// skip unexported fields
 		if ft.PkgPath != "" && !ft.Anonymous {
 			continue
 		}
-
 		col, _ := extractTag(ft.Tag.Get(fieldTagName))
-		if col != "-" && name == col {
-			return rv.Field(i), ft.Name, nil
+		key := col
+		if col == "" || col == "-" {
+			key = normFieldName(ft.Name)
 		}
-		if matchField(ft.Name, name) {
-			if col == "-" {
-				return field, "", fmt.Errorf("field corresponding to `%s' in %v cannot be set through TOML", name, rv.Type())
-			}
-			return rv.Field(i), ft.Name, nil
-		}
+		fc[key] = fieldInfo{index: ft.Index, name: ft.Name, ignored: col == "-"}
 	}
-	return field, "", fmt.Errorf("field corresponding to `%s' is not defined in %v", name, rv.Type())
+	return fc
+}
+
+func (fc fieldCache) findField(rv reflect.Value, name string) (reflect.Value, string, error) {
+	info, found := fc[normFieldName(name)]
+	if !found {
+		return reflect.Value{}, "", fmt.Errorf("field corresponding to `%s' is not defined in %v", name, rv.Type())
+	} else if info.ignored {
+		return reflect.Value{}, "", fmt.Errorf("field corresponding to `%s' in %v cannot be set through TOML", name, rv.Type())
+	}
+	return rv.FieldByIndex(info.index), info.name, nil
+}
+
+func normFieldName(s string) string {
+	return strings.Replace(strings.ToLower(s), "_", "", -1)
 }
 
 func extractTag(tag string) (col, rest string) {

decode_test.go
@@ -610,6 +610,9 @@ func TestUnmarshal_WithTable(t *testing.T) {
 	type testIgnoredFieldStruct struct {
 		Ignored string `toml:"-"`
 	}
+	type testNamedFieldStruct struct {
+		Named string `toml:"Named"`
+	}
 	type testQuotedKeyStruct struct {
 		Dog struct {
 			TaterMan struct {
@@ -776,7 +779,11 @@ d = 2`, nil,
 			err:    lineError(1, fmt.Errorf("field corresponding to `\"-\"' is not defined in toml.testIgnoredFieldStruct")),
 			expect: &testIgnoredFieldStruct{},
 		},
-
+		{
+			data:   `named = "value"`,
+			err:    lineError(1, fmt.Errorf("field corresponding to `named' is not defined in toml.testNamedFieldStruct")),
+			expect: &testNamedFieldStruct{},
+		},
 		{
 			data: `
 [a]
@@ -785,6 +792,32 @@ y = 3
 `,
 			err:    lineError(4, fmt.Errorf("field corresponding to `y' is not defined in toml.A")),
 			expect: &testStruct{},
+		},
+	})
+}
+
+func TestUnmarshal_WithEmbeddedStruct(t *testing.T) {
+	type TestEmbStructA struct {
+		A string
+	}
+	testUnmarshal(t, []testcase{
+		{
+			data: `a = "value"`,
+			expect: &struct {
+				TestEmbStructA
+				A string
+			}{
+				A: "value",
+			},
+		},
+		{
+			data: `a = "value"`,
+			expect: &struct {
+				A string
+				TestEmbStructA
+			}{
+				A: "value",
+			},
 		},
 	})
 }

decode.go
@@ -153,8 +153,9 @@ func unmarshalTable(rv reflect.Value, t *ast.Table, toplevelMap bool) error {
 	}
 	switch {
 	case rv.Kind() == reflect.Struct:
+		fc := makeFieldCache(rv.Type())
 		for key, fieldAst := range t.Fields {
-			fv, fieldName, err := findField(rv, key)
+			fv, fieldName, err := fc.findField(rv, key)
 			if err != nil {
 				return lineError(fieldLineNumber(fieldAst), err)
 			}

commit 4256b1796da6946572c4b2be4e6a6ef59dad5a69

Unbreak field matching one more time
d95c95d4f9 broke decoding of fields where the name assigned through the
struct tag contained _.

util.go
@@ -9,7 +9,10 @@ import (
 const fieldTagName = "toml"
 
 // fieldCache maps normalized field names to their position in a struct.
-type fieldCache map[string]fieldInfo
+type fieldCache struct {
+	named map[string]fieldInfo // fields with an explicit name in tag
+	auto  map[string]fieldInfo // fields with auto-assigned normalized names
+}
 
 type fieldInfo struct {
 	index   []int
@@ -18,7 +21,7 @@ type fieldInfo struct {
 }
 
 func makeFieldCache(rt reflect.Type) fieldCache {
-	fc := make(fieldCache)
+	named, auto := make(map[string]fieldInfo), make(map[string]fieldInfo)
 	for i := 0; i < rt.NumField(); i++ {
 		ft := rt.Field(i)
 		// skip unexported fields
@@ -26,17 +29,21 @@ func makeFieldCache(rt reflect.Type) fieldCache {
 			continue
 		}
 		col, _ := extractTag(ft.Tag.Get(fieldTagName))
-		key := col
+		info := fieldInfo{index: ft.Index, name: ft.Name, ignored: col == "-"}
 		if col == "" || col == "-" {
-			key = normFieldName(ft.Name)
+			auto[normFieldName(ft.Name)] = info
+		} else {
+			named[col] = info
 		}
-		fc[key] = fieldInfo{index: ft.Index, name: ft.Name, ignored: col == "-"}
 	}
-	return fc
+	return fieldCache{named, auto}
 }
 
 func (fc fieldCache) findField(rv reflect.Value, name string) (reflect.Value, string, error) {
-	info, found := fc[normFieldName(name)]
+	info, found := fc.named[name]
+	if !found {
+		info, found = fc.auto[normFieldName(name)]
+	}
 	if !found {
 		return reflect.Value{}, "", fmt.Errorf("field corresponding to `%s' is not defined in %v", name, rv.Type())
 	} else if info.ignored {

decode_test.go
@@ -611,7 +611,7 @@ func TestUnmarshal_WithTable(t *testing.T) {
 		Ignored string `toml:"-"`
 	}
 	type testNamedFieldStruct struct {
-		Named string `toml:"Named"`
+		Named string `toml:"Named_Field"`
 	}
 	type testQuotedKeyStruct struct {
 		Dog struct {
@@ -727,6 +727,10 @@ d = 2`, nil,
 					},
 				},
 			}},
+		{
+			data:   `Named_Field = "value"`,
+			expect: &testNamedFieldStruct{Named: "value"},
+		},
 		{
 			data: string(loadTestData("unmarshal-table-withmap.toml")),
 			expect: &testStructWithMap{