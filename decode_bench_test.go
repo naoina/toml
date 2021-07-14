329a1f2d4870e82e78d84b09b4cb1bbc474f4b05
Felix Lange <fjl@twurst.com>
2021-07-14 11:33:55 +0200

    Report MB/s in decoder benchmark

--- decode_bench_test.go
+++ decode_bench_test.go
@@ -8,6 +8,7 @@ func BenchmarkUnmarshal(b *testing.B) {
 	var v testStruct
 	data := loadTestData("test.toml")
 	b.ResetTimer()
+	b.SetBytes(int64(len(data)))
 
 	for i := 0; i < b.N; i++ {
 		if err := Unmarshal(data, &v); err != nil {
